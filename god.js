e8c35f0f9b13a514955c66aaa370d469703122ab
Molnár István <33826380+molnaristvan95@users.noreply.github.com>
2021-01-22 23:56:10 +0100

    Update god.js

--- god.js
+++ god.js
@@ -1,2 +1,42 @@
+console.clear();
 //noprotect
-console.log('asd');
+
+var logOb = {};
+
+function slideDownOnce(id, time, funcId, how, delay, px, more) {
+  var elem = document.getElementById(id),
+      max = elem.scrollHeight,
+      fontStr = window.getComputedStyle(elem).getPropertyValue('font-size'),
+      fontNum = fontStr.substring(fontStr.lenght, 2),
+      fontInt = parseFloat(parseInt(fontNum, 10)),
+      sHeight = (fontInt * more);
+  elem.style.height = sHeight + 'px';
+  elem.style.overflow = 'hidden hidden';
+  logOb[('downInter' + funcId)] = null;
+  logOb['up' + funcId] = fontInt * more;
+  if (how === 'auto') {
+    window.setTimeout(function(){
+      logOb[('inter' + funcId)] = window.setInterval(function(){
+        slideDownOnceEngine(id, logOb['up' + funcId], max, funcId, px);
+      }, time);
+    }, delay);
+  } else {
+    elem.addEventListener('click', function() {
+      window.setTimeout(function(){
+        logOb['downInter' + funcId] = window.setInterval(function(){
+          slideDownOnceEngine(id, logOb['up' + funcId], max, funcId, px);
+        }, time);
+      }, delay);
+    });
+  }
+}
+
+function slideDownOnceEngine(elem, start, finish, funcId, px) {
+  if (document.getElementById(elem).scrollHeight > finish) {
+    document.getElementById(elem).scrollHeight = finish + 'px';
+    return window.clearInterval(logOb[('downInter' + funcId)]);
+  } else {
+    logOb['up' + funcId] += px;
+    document.getElementById(elem).style.height = logOb['up' + funcId] + 'px';
+  }
+}
